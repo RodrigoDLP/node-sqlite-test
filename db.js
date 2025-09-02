23b60691e8e98d2a641e26bb26b935b68489e0f1
RodrigoDLP <rodrigo.delapuente@utec.edu.pe>
2025-09-01 23:47:00 -0500

    Removed original learning purpose comments

--- db.js
+++ db.js
@@ -1,20 +1,3 @@
-/*const path = require('path');
-const sqlite = require('sqlite3');
-const db = new sqlite.Database(path.resolve(__dirname, '../../database.db'), sqlite.OPEN_READWRITE, (error)=>{
-    if (error) {return console.error(error);}})
-
-const studentstablequery = `CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname VARCHAR(20) NOT NULL, lastname VARCHAR(50) NOT NULL);`;
-const teacherstablequery = `CREATE TABLE IF NOT EXISTS teachers (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname VARCHAR(20) NOT NULL, lastname VARCHAR(50) NOT NULL);`;
-const subjectstablequery = `CREATE TABLE IF NOT EXISTS subjects (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(100) NOT NULL);`
-const enrollmentstablequery = `CREATE TABLE IF NOT EXISTS enrollments (studentid INTEGER NOT NULL, subjectid INTEGER NOT NULL, teacherid INTEGER NOT NULL, semester VARCHAR(10) NOT NULL,
-FOREIGN KEY (studentid) REFERENCES students(id), FOREIGN KEY (teacherid) REFERENCES teachers(id), FOREIGN KEY (subjectid) REFERENCES subjects(id));`;
-
-db.run(studentstablequery, (error)=>{if (error) return console.error(error);});
-db.run(teacherstablequery, (error)=>{if (error) return console.error(error);});
-db.run(subjectstablequery, (error)=>{if (error) return console.error(error);});
-db.run(enrollmentstablequery, (error)=>{if (error) return console.error(error);});*/
-
-
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const { promisify } = require('util');
--- index.js
+++ index.js
@@ -1,52 +1,3 @@
-/*
-const sqlite = require('sqlite3');
-
-const db = new sqlite.Database('./data.db', sqlite.OPEN_READWRITE, (error)=>{
-    if (error) {console.error(error);}
-});
-
-/*
-sql = `CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(100) NOT NULL)`;
-db.run(sql, (error)=>{
-    if (error) {console.error(error);}
-})*/
-
-
-/* sql = `INSERT INTO categories (name) values (?)`;
-db.run(sql, ["Categoría 4"], (error)=>{
-    if (error) {console.error(error);}
-}); */
-
-/* sql = `UPDATE categories SET name = ? WHERE id = ?`;
-db.run(sql, ["Categoría 1B", 1], (error)=>{
-    if (error) {console.error(error);}
-}); */
-/*sql = `DELETE FROM categories WHERE id = ?`;
-db.run(sql, [2], (error)=>{if (error) {console.error(error);}});*/
-
-//imprimir toda la tabla
-
-/*
-sql = `SELECT * FROM categories`;
-db.all(sql, (error, rows)=>{
-    if (error) {console.error(error);}
-    else {console.log(rows);}
-})
-*/
-
-
-//select by id
-/*
-sql = `SELECT * FROM categories WHERE id = ?`;
-db.all(sql, [3], (error, rows)=>{
-    if (error) {console.error(error);}
-    else {console.log(rows);}
-});
-*/
-/*
-//cerrado de base de datos, siempre al final
-db.close((error)=>{if (error) {console.error(error);}});*/
-
 const express = require('express');
 const {db, init} = require('./db');
 const studentController = require('./students');
@@ -65,7 +16,7 @@ app.use('/enrollments', enrollmentController);
 init()
   .then(() => {
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, ()=> console.log(`http://localhost:${PORT}`));
+    app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
   })
   .catch(err=>{
     console.error('Init error:', err);
